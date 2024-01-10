283f354b2447e68a40ba9fc33743ef589d074aef
Ava Byrne <153855870+trawl-rye0t@users.noreply.github.com>
2024-01-10 07:28:44 +0100

    Create Customer.java

---
+++ Customer.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Customer {
+    private String name;
+    private List<Account> accounts;
+
+    public Customer(String name) {
+        this.name = name;
+        this.accounts = new ArrayList<>();
+    }
+
+    public void addAccount(Account account) {
+        accounts.add(account);
+    }
+}
