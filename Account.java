64b2ce79c1db815ca2b7d8e11cb68907fc31045d
Ava Byrne <153855870+trawl-rye0t@users.noreply.github.com>
2024-01-10 07:28:54 +0100

    Create Account.java

---
+++ Account.java
@@ -0,0 +1,19 @@
+public class Account {
+    private double balance;
+
+    public Account(double initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        balance -= amount;
+    }
+}
