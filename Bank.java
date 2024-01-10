78f7567355c13abe6e0f30a9cb04d3478eb4e914
Ava Byrne <153855870+trawl-rye0t@users.noreply.github.com>
2024-01-10 07:28:33 +0100

    Create Bank.java

---
+++ Bank.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bank {
+    private List<Customer> customers;
+
+    public Bank() {
+        this.customers = new ArrayList<>();
+    }
+
+    public void addCustomer(Customer customer) {
+        customers.add(customer);
+    }
+
+    public void processTransaction(Transaction transaction) {
+        transaction.process();
+    }
+}
